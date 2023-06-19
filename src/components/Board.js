3d123225773ba2a3c72c394c1074ed67f9895b21
depayse <cbs5295@gmail.com>
2023-06-19 21:02:00 +0900

    TicTacToe 앱 완성

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,31 +1,32 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import "./Board.css";
 import Square from './Square';
 
-export default class Board extends Component {
-  renderSquare(i) {
-    return <Square value={i} />
+const Board = ({squares, onClick}) => {
+
+  
+  const renderSquare = (i) => {
+    return <Square value={squares[i]} onClick = {() => onClick(i)} />
   }
-  render() {
-    return (
-      <div>
-        <div className='status'>Next Player: X, O</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+  return (
+    <div className="board-wrapper">
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-    )
-  }
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  )
 }
+
+export default Board
